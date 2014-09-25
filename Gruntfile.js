9181bd358c4dbe633a2b5091c1ce409625ee5774
Eduard Pelesh <edpelesh@gmail.com>
2014-09-25 10:33:13 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,267 @@
+// global module:false
+var path = require('path');
+
+// createConfig script for replacing Smarty {$STATIC_URL} references when building config for concat
+function createSmartyStaticConcatConfig(context, block) {
+	var cfg = {files: []};
+	var staticPattern = /\{\$\s*STATIC_URL\s*\}/;
+
+	block.dest = block.dest.replace(staticPattern, '');
+	var outfile = path.join(context.outDir, block.dest);
+
+	var files = {
+		dest: outfile,
+		src: []
+	};
+	context.inFiles.forEach(function(f) {
+		files.src.push(path.join(context.inDir, f.replace(staticPattern, '')));
+	});
+	cfg.files.push(files);
+	context.outFiles = [block.dest];
+	return cfg;
+};
+
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+
+		baseDir: 'static',
+		sourceDir: '<%= baseDir %>/src',
+		publicDir: '<%= baseDir %>/pub',
+
+		watch: {
+			options: {
+				livereload: true
+			},
+			less: {
+				files: ['<%= sourceDir %>/less/**/*.less'],
+				tasks: ['less:dev']
+			},
+			includes: {
+				files: ['<%= baseDir %>/layouts/_includes/**/*'],
+				tasks: ['preprocess:html']
+			},
+			jshint: {
+				files: ['<%= sourceDir %>/js/**/*.js'],
+				tasks: ['jshint:all']
+			}
+		},
+
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+			},
+			all: ['<%= sourceDir %>/js/**/*.js', '!<%= sourceDir %>/js/vendor/**/*.js']
+		},
+
+		spritesheet: {
+			assets: {
+				options: {
+					outputCss: '/less/assets.less',
+					selector: '.icon',
+
+					output: {
+						legacy: {
+							pixelRatio: 1,
+							outputImage: '/img/sprites/assets.png',
+							filter: function (fullpath) {
+								return fullpath.indexOf('@2x') === -1;
+							}
+						},
+
+						retina: {
+							pixelRatio: 2,
+							outputImage: '/img/sprites/assets@2x.png',
+							filter: function (fullpath) {
+								return fullpath.indexOf('@2x') >= 0;
+							}
+						}
+					},
+
+					resolveImageSelector: function (name, fullpath) {
+						return name.split('@2x').join('');
+					}
+				},
+				files: {
+					'<%= sourceDir %>': '<%= sourceDir %>/img/assets/*.png'
+				}
+			}
+		},
+
+		less: {
+			dev: {
+				options: {
+					sourceMap: true,
+					sourceMapFilename: 'static/src/css/app.css.map',	// Path to save map
+					sourceMapBasepath: 'static',											// Deleting excessive prefixes
+					sourceMapRootpath: '/'														// Path prefix (*.map file)
+				},
+				files: {
+					'<%= sourceDir %>/css/app.css': ['<%= sourceDir %>/less/app.less']
+				}
+			},
+			production: {
+				options: {
+					sourceMap: false,
+					compress: true,
+					cleancss: true
+				},
+				files: {
+					'<%= publicDir %>/css/app.min.css': ['<%= sourceDir %>/less/app.less']
+				}
+			}
+		},
+
+		preprocess: {
+			options: {
+				context : {
+					DEBUG: true
+				}
+			},
+			html: {
+				files: [{
+					expand: true,
+					cwd: '<%= baseDir %>/layouts/_includes',
+					src: 'tpl.*',
+					dest: '<%= baseDir %>/layouts/',
+					rename: function (dest, src) {
+						return dest + src.replace('tpl.', '');
+					}
+				}]
+			}
+		},
+
+		clean: {
+			css: ['<%= publicDir %>/css'],
+			js: ['<%= publicDir %>/js'],
+			img: ['<%= publicDir %>/img']
+		},
+
+		copy: {
+
+			css: {
+				src: 'engine/application/view/themes/_default/_sources/css-files.smt',
+				dest: 'engine/application/view/themes/_default/_sources/css-files.min.smt'
+			},
+
+			js: {
+				src: 'engine/application/view/themes/_default/_sources/js-files.smt',
+				dest: 'engine/application/view/themes/_default/_sources/js-files.min.smt'
+			},
+
+			img: {
+				cwd: '<%= sourceDir %>',																												// root to copy
+				src: ['img/**/*', '!img/assets/**', '!img/ignore/**', '!img/**/zzz*'],							// copy folder
+				dest: '<%= publicDir %>/',
+				expand: true																																		// required when using cwd
+			}
+		},
+
+		useminPrepare: {
+			js: 'engine/application/view/themes/_default/_sources/js-files.smt',
+			css: 'engine/application/view/themes/_default/_sources/css-files.smt',
+			options: {
+				root: '<%= baseDir %>',
+				staging: '<%= baseDir %>/.tmp/',
+				dest: '<%= publicDir %>',
+				flow: { // remove {$STATIC} from *.smt
+					steps: {
+						js: [{
+								name: 'concat',
+								createConfig: createSmartyStaticConcatConfig
+							},
+							'uglifyjs'
+						]},
+					post: {} // REQUIRED! DO NOT REMOVE
+				}
+			}
+		},
+
+		filerev: { // change versions (only if was chages!)
+			options: {
+				encoding: 'utf8',
+				algorithm: 'md5',
+				length: 8
+			},
+			css: {
+				src: '<%= publicDir %>/css/*.css'
+			},
+			js: {
+				src: '<%= publicDir %>/js/*.js'
+			},
+			img: {
+				src: '<%= publicDir %>/img/**/*.{jpg,jpeg,png}'
+			}
+		},
+
+		usemin: {
+			css: 'engine/application/view/themes/_default/_sources/css-files.min.smt',
+			js: 'engine/application/view/themes/_default/_sources/js-files.min.smt',
+			img: '<%= publicDir %>/css/**/*.css',
+			options: {
+				assetsDirs: ['<%= publicDir %>', '<%= publicDir %>/css', '<%= publicDir %>/js', '<%= publicDir %>/img/sprites'],
+				patterns: {
+					css: [
+						[/(css\/app\.min\.css)/, 'Replace CSS references']
+					],
+					js: [
+						[/(js\/app\.min\.js)/, 'Replace JS references']
+					],
+					img: [
+						[/(img\/.*?\.(?:jpg|jpeg|png))/img, 'Image src replacement in css files']
+					]
+				},
+				blockReplacements: {
+					js: function (block) {
+						return '<script src="{$STATIC_URL}pub' + block.dest + '"></script>';
+					},
+					css: function (block) {
+						return '<link rel="stylesheet" href="{$STATIC_URL}pub' + block.dest + '">';
+					}
+				}
+			}
+		}
+
+	});
+
+	grunt.loadTasks(path.join(__dirname, 'node_modules', 'node-spritesheet', 'tasks'));
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-preprocess');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-filerev');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('sprite', ['spritesheet']);
+	grunt.registerTask('build-css', [
+		'clean:css',
+		'copy:css',
+		'less:production',
+		'useminPrepare:css',
+		'filerev:css',
+		'usemin:css'
+	]);
+	grunt.registerTask('build-js', [
+		'clean:js',
+		'copy:js',
+		'useminPrepare:js',
+		'concat',
+		'uglify',
+		'filerev:js',
+		'usemin:js'
+	]);
+	grunt.registerTask('rev-img', [
+		'clean:img',
+		'copy:img',
+		'filerev:img',
+		'usemin:img'
+	]);
+	grunt.registerTask('build', ['build-css', 'rev-img', 'build-js']);
+	grunt.registerTask('live', ['watch']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-ideil-atom
-==========
+IDEIL | ATOM
+========================
 
-Boilerplate structure and build used by ideil team
+ATTENTION! GO AHEAD!
+--------------------
+
+# Update your template: http://html5boilerplate.com/.
+
+- Non processed init.html path: "evoAtom/static/layouts/includes/tpl.init.html".
+- Rewrite excess default demo includes: "evoAtom/static/layouts/includes/*.html".
+
+# Chek (replace) default *.less files constructor in app.less file: "evoAtom/static/src/less/app.less".
+
+- Default *.less files path: evoAtom/static/src/less/**/*.less.
+
+# Download and install latest Bootstrap (*.less, *.js files): https://github.com/twbs/bootstrap/archive/.
+
+- Bootstrap *.less files path: evoAtom/static/src/less/vendor/bootstrap/**/*.less.
+- Bootstrap *.js files path: evoAtom/static/src/js/vendor/bootstrap/*.js.
+
+# Update all default *.js files to stabel versions: evoAtom/static/src/js/vendor/**/*.js.
+(Don't forget rewrite versions in html includes evoAtom/static/layouts/includes/*.html).
+
+- modernizr (boilerplate): head (evoAtom/static/layouts/includes/head.html): .
+- slider (flexslider): (evoAtom/static/layouts/includes/js.html) http://flexslider.woothemes.com/.
+- bootstrap (evoAtom/static/layouts/includes/js.html).
+- handlebars (evoAtom/static/layouts/includes/js.html).
+
+# ".editorconfig", ".gitignore", ".jshintrc": names of this files must be w/o first "-" so remove it.
+
+# Give the name of new grunt project in package.json file (replace "***PROJECT_NAME***").
+
+# Install Node Packaged Modules: "npm install" (terminal).
+
+# Start Grunt: "grunt watch" (terminal).
+
+# READY STEADY GO!
