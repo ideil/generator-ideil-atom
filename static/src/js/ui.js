d6e314ffa0c982063b9b83c9c5dfefeae77d6e50
Serhiy Vlasiuk <s.vlasiuk@ideil.com>
2015-04-29 17:05:45 +0300

    added: custom fileupload; sticky header; slick default js init script; is-loading state; is-required state; custom select styles. updated: dl-extensions.

--- static/src/js/ui.js
+++ static/src/js/ui.js
@@ -1,2 +1,22 @@
 /*global */
 /*jshint devel:true */
+
+(function ($, window, undefined) {
+	// console.log('Hi world!');
+
+	// $('.js-slick-default').slick({
+	// 	slide: 'li',
+	// 	prevArrow: '<button class="slick-prev"><span class="icon"></span></button>',
+	// 	nextArrow: '<button class="slick-next"><span class="icon"></span></button>',
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 3
+
+	// 	// dots: true,
+	// 	// arrows: false,
+	// 	// autoplay: true,
+	// 	// autoplaySpeed: 2000,
+	// 	// centerMode: true,
+	// 	// variableWidth: true,
+	// 	// infinite: false,
+	// });
+})(jQuery, window);
